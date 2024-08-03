7d9e9485c6ed18b0624096b297732631f7d136a2
BuilderSIA <lordallplay@gmail.com>
2024-08-04 01:11:56 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import "./App.css";
+import "./styles/res1024.css";
+import "./styles/res768.css";
+import "./styles/res600.css";
+import Navbar from "./components/Navbar";
+import Hero from "./pages/Hero";
+import Home from "./components/Home";
+import Categoriesfor from "./pages/Categoriesfor";
+import Results from "./pages/Results";
+import Grants from "./pages/Grants";
+import { useTranslation } from "react-i18next";
+
+function App() {
+  const language = localStorage.getItem("i18nextLng");
+  const { t, i18n } = useTranslation();
+  const handleChange = (event) => {
+    const selectedLang = event.target.value;
+    i18n.changeLanguage(selectedLang);
+  };
+  return (
+    <>
+      <Navbar
+        handleChange={handleChange}
+        t={t}
+        i18n={i18n}
+        language={language}
+      />
+      <Home />
+      <Hero t={t} />
+      <Categoriesfor t={t} />
+      <Results t={t} />
+      <Grants t={t} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Ourhelp.jsx
@@ -0,0 +1,16 @@
+
+const Ourhelp = () => {
+  return (
+    <div className="ourhelp">
+        <div className="ourhelp-cont">
+            <div className="ourhelp-text">
+                <h2>
+                    
+                </h2>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Ourhelp
