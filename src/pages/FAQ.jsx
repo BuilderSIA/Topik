a0754d9698095014c2c2569d8fd47e19b9b94671
BuilderSIA <lordallplay@gmail.com>
2024-08-07 09:45:37 +0500

    final

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -30,13 +30,13 @@ const FAQ = ({t}) => {
                 <h2 className="accordion-header">
                   <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#coll2" aria-expanded="false" aria-controls="coll2">
                     <h5>
-                      {t("faqtext")}
+                      {t("faqtext2")}
                     </h5>
                   </button>
                 </h2>
                 <div id="coll2" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                   <div className="accordion-body">
-                    
+                  {t("faqans2")}
                   </div>
                 </div>
               </div>
@@ -45,13 +45,13 @@ const FAQ = ({t}) => {
                 <h2 className="accordion-header">
                   <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#coll3" aria-expanded="false" aria-controls="coll3">
                     <h5>
-                    {t("faqtext")}
+                    {t("faqtext3")}
                     </h5>
                   </button>
                 </h2>
                 <div id="coll3" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                   <div className="accordion-body">
-                    
+                  {t("faqans3")}
                   </div>
                 </div>
               </div>
@@ -60,13 +60,13 @@ const FAQ = ({t}) => {
                 <h2 className="accordion-header">
                   <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#coll4" aria-expanded="false" aria-controls="coll4">
                     <h5>
-                    {t("faqtext")}
+                    {t("faqtext4")}
                     </h5>
                   </button>
                 </h2>
                 <div id="coll4" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                   <div className="accordion-body">
-                    
+                  {t("faqans4")}
                   </div>
                 </div>
               </div>
