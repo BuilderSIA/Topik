aef673ee7456df51f7d98487e36e4a1187b479c4
BuilderSIA <lordallplay@gmail.com>
2024-08-07 07:57:03 +0500

    detector

--- src/i18n.jsx
+++ src/i18n.jsx
@@ -1,9 +1,9 @@
 import i18n from "i18next";
-import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
 import I18NextHttpBackend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 import uztranslation from '../public/locales/uzb.json'
 import rutranslation from '../public/locales/rus.json'
+import i18nextBrowserLanguagedetector from "i18next-browser-languagedetector";
 
 
 const lang = localStorage.getItem("i18nextLng") || "uz";
@@ -15,7 +15,7 @@ const lang = localStorage.getItem("i18nextLng") || "uz";
 
 i18n
 .use(I18NextHttpBackend)
-.use(I18nextBrowserLanguageDetector)
+.use(i18nextBrowserLanguagedetector)
 .use(initReactI18next)
 .init({
     fallbackLang: 'uz',
